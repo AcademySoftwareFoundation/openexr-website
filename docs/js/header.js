580d4e8023649ee2aeea755a816d7581759280c6
smartin-13 <61707536+smartin-13@users.noreply.github.com>
2020-08-03 16:49:51 -0700

    Updates 8/3/2020
    Added PATENTS Page
    Linked DOCUMENTATION directly to wiki
    Made header and nav into js
    INDEX: made news and download into a two column layout
    Updated OpenEXR logo and changed the "red" in the headers to match.

---
+++ docs/js/header.js
@@ -0,0 +1,18 @@
+document.write('\
+<head>\
+	<meta charset="utf-8">\
+	<title>OpenEXR</title>\
+	<link rel="stylesheet" href="stylesheets/main.css">\
+	<script src="js/popUp.js" language="javascript"> </script>\
+	<script src="js/headerScroll.js" language="javascript"> </script>\
+</head>\
+<header class= "collapse-header">\
+	<aside class="header-logo" id="collapse-logo">\
+		<img class="logo-img" id="logo-img" src="images/logos/openexr-stacked-color.png" >\
+	</aside>\
+</header>\
+<div>\
+	<div class="scroll-banner">\
+	</div>\
+</div>\
+')
---
+++ docs/js/navigation.js
@@ -0,0 +1,31 @@
+document.write('\
+<div class="sticky-pos">\
+	<nav id="nav-container">\
+		<ul class="nav-ul-container">\
+			<li class="nav-text">\
+				<a href="index.html">Home</a>\
+			</li>\
+			<li class="nav-text">\
+				<a href="about.html">About OpenEXR</a>\
+			</li>\
+			<li class="nav-text">\
+				<a href="download.html">Download</a>\
+			</li>\
+			<li class="nav-text">\
+				<a href="https://github.com/smartin-13/openexr-website/wiki/OpenEXR-Wiki">Documentation</a>\
+			</li>\
+			<li class="nav-text">\
+				<a href="images.html">Images</a>\
+			</li>\
+			<li class="nav-text">\
+				<a href="security.html">Security</a>\
+			</li>\
+			<li class="nav-text">\
+				<a href="news.html">News</a>\
+			</li>\
+			<li class="nav-text">\
+				<a href="help.html">Help</a>\
+			</li>\
+		</ul>\
+	</nav>\
+</div>')
