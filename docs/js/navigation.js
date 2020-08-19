1ab051a72064b016206203cd23f71fa8871ddd50
smartin-13 <61707536+smartin-13@users.noreply.github.com>
2020-08-19 11:22:47 -0700

    Removed "OpenEXR" from About

--- docs/js/navigation.js
+++ docs/js/navigation.js
@@ -6,7 +6,7 @@ document.write('\
 				<a href="index.html">Home</a>\
 			</li>\
 			<li class="nav-text">\
-				<a href="about.html">About OpenEXR</a>\
+				<a href="about.html">About</a>\
 			</li>\
 			<li class="nav-text">\
 				<a href="download.html">Download</a>\
